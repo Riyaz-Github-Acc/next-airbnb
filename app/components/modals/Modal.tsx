2f89f0c44e77f011c3b2d66f6699809e5b4dd4b3
Riyaz-Github-Acc <riyazofficial0@gmail.com>
2023-06-03 04:38:27 +0530

    small change in modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -70,10 +70,32 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <>
       <div
-        className="flex flex-col items-center justify-center overflow-x-hidden overflow-y-auto
-      inset-0 fixed z-50 outline-none focus:outline-none bg-neutral-800/70"
+        className="
+        flex
+        flex-col
+        items-center
+        justify-center
+        overflow-x-hidden
+        overflow-y-auto
+        inset-0
+        fixed
+        z-50
+        outline-none
+        focus:outline-none
+        bg-neutral-800/70
+      "
       >
-        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/6 mx-auto lg:h-auto md:h-auto">
+        <div
+          className="
+          relative
+          w-full
+          md:w-4/6
+          lg:w-3/6
+          xl:w-2/6
+          mx-auto
+          lg:h-auto
+          md:h-auto"
+        >
           {/*content*/}
           <div
             className={`
@@ -107,7 +129,7 @@ const Modal: React.FC<ModalProps> = ({
                 className="
                 flex 
                 items-center 
-                p-6
+                p-4
                 rounded-t
                 justify-center
                 relative
