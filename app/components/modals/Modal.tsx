25a4d332609bcb6b4f837a8cf69e9955b0a963f6
Riyaz-Github-Acc <riyazofficial0@gmail.com>
2023-06-03 05:40:57 +0530

    final changes

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -149,7 +149,7 @@ const Modal: React.FC<ModalProps> = ({
               {/*body*/}
               <div className="relative p-6 flex-auto">{body}</div>
               {/*footer*/}
-              <div className="flex flex-col gap-2 p-6">
+              <div className="flex flex-col gap-2 px-6 pb-5">
                 <div
                   className="
                     flex 
